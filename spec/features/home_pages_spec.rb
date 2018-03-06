935c0a260aeb2ad9374a3b46bf21ac48a76f878f
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-03-06 19:02:23 +0000

    home page spec created, using capybara

---
+++ spec/features/home_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do 
+    # puts Product.all.inspect
+    # ACT
+    visit root_path
+
+    # sleep 1
+
+
+    # DEBUG / VERIFY
+    save_screenshot
+    
+    # puts page.html
+    expect(page).to have_css 'article.product', count: 10
+
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,20 +13,20 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'database_cleaner'
+# require 'database_cleaner'
 require 'factory_girl_rails'
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
+  # config.around(:each) do |example|
+  #   DatabaseCleaner.cleaning do
+  #     example.run
+  #   end
+  # end
 
   config.include FactoryGirl::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
